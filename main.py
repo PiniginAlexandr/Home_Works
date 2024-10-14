ed8e26887699928780dd822ed3b8b77aa3b4d445
PiniginAlexandr <pul9dur2@gmail.com>
2024-10-14 22:38:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+print('1st program\n', (9 ** 0.5) * 5)
+
+print('2nd program\n', 9.99 > 9.98 and 1000 != 1000.1)
+
+print('3rd program\n', 2 * 2 + 2 == 2 * (2 + 2))
+
+print('4th program\n', int(float('123.456') * 10) % 10)
+
+
