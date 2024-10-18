80795fe53be46af00668b5125ec1f62ff4e0f5da
PiniginAlexandr <pul9dur2@gmail.com>
2024-10-18 19:30:39 +0300

    ДЗ Переменные

---
+++ Переменные.py
@@ -0,0 +1,8 @@
+home_work = 12
+hours = 1.5
+python = 'Python'
+time = hours / home_work
+print(f'{python} '
+      f'\nВсего задач: {home_work} '
+      f'\nЗатрачено часов: {hours} '
+      f'\nСреднее время выполнения {time} часа.')
