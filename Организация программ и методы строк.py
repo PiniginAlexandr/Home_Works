eb45f32c4f3f42ccb59764ae2e3794c1e9633086
PiniginAlexandr <pul9dur2@gmail.com>
2024-10-19 16:07:26 +0300

    module_1_4

---
+++ Организация программ и методы строк.py
@@ -0,0 +1,7 @@
+my_string = input('Введите текст: ')
+print('Количество символом в строке:', len(my_string))
+print(f'Строка в верхнем регистре: \n    {my_string.upper()}')
+print(f'Строка в нижнем регистре: \n    {my_string.lower()}')
+print(f'Удаление всех пробелов: \n    {my_string.replace(' ', '')}')
+print(f'Первый символ строки: \n    {my_string[0]}')
+print(f'Последний символ строки: \n    {my_string[-1]}')
